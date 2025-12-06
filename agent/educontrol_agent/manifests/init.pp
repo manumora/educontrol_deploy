180157f34a62f8658cbea2568e1a43658ad8b0bd
Manuel Mora Gordillo <manuito@gmail.com>
2025-12-06 20:13:05 +0100

    Updated package

--- agent/educontrol_agent/manifests/init.pp
+++ agent/educontrol_agent/manifests/init.pp
@@ -27,7 +27,7 @@
 class educontrol_agent () {
 
   # Versión del agente
-  $version = '1.0.4'
+  $version = 'AQUI_NUEVA_VERSION'
 
   # Asegurar que el directorio existe
   file { '/etc/educontrol':
